f7737bc19abcf6621eba892028eb9f8878998a6c
Juan Ignacio Guerrero <49444608+Juanigue@users.noreply.github.com>
2023-09-13 15:51:44 -0300

    Add files via upload

--- transactions.sql
+++ transactions.sql
@@ -1,5 +1,34 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-09-2023 a las 20:51:23
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `rol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transactions`
+--
+
 CREATE TABLE `transactions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `item_number` varchar(50) DEFAULT NULL,
   `item_name` varchar(255) DEFAULT NULL,
   `item_price` float(10,2) DEFAULT NULL,
@@ -17,6 +46,38 @@ CREATE TABLE `transactions` (
   `payment_source` varchar(50) DEFAULT NULL,
   `payment_status` varchar(25) NOT NULL,
   `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  `modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `item_number`, `item_name`, `item_price`, `item_price_currency`, `payer_id`, `payer_name`, `payer_email`, `payer_country`, `merchant_id`, `merchant_email`, `order_id`, `transaction_id`, `paid_amount`, `paid_amount_currency`, `payment_source`, `payment_status`, `created`, `modified`) VALUES
+(1, 'DP12345', 'Demo Product', 75.00, 'USD', '9Y3E23CM8EDF6', 'John Doe', 'sb-543x5u27380172@personal.example.com', 'AR', 'WLR8RX9AKZCR6', 'sb-d47z47v27381421@business.example.com', '9ML56848W4459215X', '34K32215XK106234A', 75.00, 'USD', 'paypal', 'COMPLETED', '2023-09-13 17:10:12', '2023-09-13 12:10:57'),
+(2, 'DP12345', 'Demo Product', 75.00, 'USD', '9Y3E23CM8EDF6', 'John Doe', 'sb-543x5u27380172@personal.example.com', 'AR', 'WLR8RX9AKZCR6', 'sb-d47z47v27381421@business.example.com', '2D568113YL4276903', '4UK24824A61603735', 75.00, 'USD', 'paypal', 'COMPLETED', '2023-09-13 17:13:57', '2023-09-13 12:14:21');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
